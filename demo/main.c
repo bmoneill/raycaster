3c897bcb389e357ee6daa3d15524de65338cfed7
Ben O'Neill <ben@oneill.sh>
2025-10-06 15:36:24 -0400

    Update demo

--- demo/main.c
+++ demo/main.c
@@ -1,5 +1,6 @@
 #include "raycast.h"
 
+#include <getopt.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,30 +11,18 @@
 #define GREEN 0xFF00FF00
 #define BLUE 0xFF0000FF
 
-int demoMap[] = {
-    GREEN, GREEN, PURPLE, PURPLE, PURPLE,  PURPLE,  PURPLE,  PURPLE,  PURPLE,  PURPLE,
-    BLUE,    -1,    -1,    -1,    -1,    -1,    -1,    -1,    -1,    BLUE,
-    BLUE,  RED,   BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  -1,    BLUE,
-    RED,    -1,    -1,    -1,    -1,    -1,    -1,    -1,    -1,    BLUE,
-    RED,    -1,    GREEN, GREEN, GREEN, GREEN, GREEN, -1,    -1,    BLUE,
-    PURPLE,    -1,    -1,    -1,    -1,    -1,    -1,    -1,    -1,    BLUE,
-    BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  BLUE,  BLUE,
-};
+static void demo      (int,  int, int, int);
+static int* expand_map(int*, int, int, int, int);
 
-int* expand_map(int *map, int w, int h, int nw, int nh) {
-    int *newMap = (int *) malloc((nw * nh) * sizeof(int));
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            int color = map[y * w + x];
-            for (int j = 0; j < nh / h; j++) {
-                for (int i = 0; i < nw / w; i++) {
-                    newMap[(y * (nh / h) + j) * nw + (x * (nw / w) + i)] = color;
-                }
-            }
-        }
-    }
-    return newMap;
-}
+int demoMap[] = {
+    GREEN,  GREEN,  PURPLE, PURPLE, PURPLE, PURPLE, PURPLE, PURPLE, PURPLE, PURPLE,
+    BLUE,   -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,     BLUE,
+    BLUE,   PURPLE, BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   -1,     BLUE,
+    PURPLE, -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,     BLUE,
+    PURPLE, -1,     GREEN,  GREEN,  GREEN,  GREEN,  GREEN,  -1,     -1,     BLUE,
+    PURPLE, -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,     BLUE,
+    BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   BLUE,   BLUE,
+};
 
 int main(int argc, char *argv[]) {
     int w = 800;
@@ -41,12 +30,29 @@ int main(int argc, char *argv[]) {
     RaycastColor fg = RED;
     RaycastColor bg = BLACK;
 
+    int opt;
+    while ((opt = getopt(argc, argv, "w:h:f:b:")) != -1) {
+        switch (opt) {
+            case 'w': w = atoi(optarg); break;
+            case 'h': h = atoi(optarg); break;
+            case 'f': fg = (RaycastColor) strtol(optarg, NULL, 16); break;
+            case 'b': bg = (RaycastColor) strtol(optarg, NULL, 16); break;
+            default:
+                fprintf(stderr, "Usage: %s [-w width] [-h height] [-f foreground_color] [-b background_color]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    demo(w, h, fg, bg);
+    return 0;
+}
+
+static void demo(int w, int h, int fg, int bg) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Raycaster Demo", w, h, SDL_WINDOW_RESIZABLE);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
     Raycaster *raycaster = raycast_init(w, h);
-    RaycastCamera camera = {800/10 + 10, 800/6, 0.0f, 90.0f, 0.0f, 0.0f, 90};
+    RaycastCamera camera = {w/10 + 10, h/6+10, 0.0f, 90.0f, 0.0f, 0.0f, 90};
     raycaster->map = expand_map(demoMap, 10, 6, w, h);
 
     int running = 1;
@@ -93,5 +99,19 @@ int main(int argc, char *argv[]) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
+}
+
+static int* expand_map(int *map, int w, int h, int nw, int nh) {
+    int *newMap = (int *) malloc((nw * nh) * sizeof(int));
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            int color = map[y * w + x];
+            for (int j = 0; j < nh / h; j++) {
+                for (int i = 0; i < nw / w; i++) {
+                    newMap[(y * (nh / h) + j) * nw + (x * (nw / w) + i)] = color;
+                }
+            }
+        }
+    }
+    return newMap;
 }
